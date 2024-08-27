0b2c8ad4d602a21c147f2428f28cb9f6cdf6eafc
Argox2 <alessandro@debian>
2024-08-27 08:16:51 -0600

    functions for hola_paco

--- hola_paco/hola_paco.ino
+++ hola_paco/hola_paco.ino
@@ -1,79 +1,151 @@
-const int FR_ENA_ATRAS = 23;        // Rueda delantera derecha
+
+
+// Rueda delantera derecha.
+const int FR_ENA_ATRAS = 23;        
 const int FR_ENA_ADELANTE = 22;
 const int FR_PWM_ATRAS = 9;        
 const int FR_PWM_ADELANTE = 10;
 
-const int FL_ENA_ATRAS = 35;        // Rueda delantera derecha
+// Rueda delantera izquierda. 
+const int FL_ENA_ATRAS = 35;        
 const int FL_ENA_ADELANTE = 34;
 const int FL_PWM_ATRAS = 3;        
 const int FL_PWM_ADELANTE = 4;
 
-const int BR_ENA_ATRAS = 26;        // Rueda trasera derecha
+// Rueda trasera derecha
+const int BR_ENA_ATRAS = 26;        
 const int BR_ENA_ADELANTE = 27;
 const int BR_PWM_ATRAS = 6;        
 const int BR_PWM_ADELANTE = 5;
 
-const int BL_ENA_ATRAS = 30;        // Rueda trasera derecha
+// Rueda trasera izquierda 
+const int BL_ENA_ATRAS = 30;        
 const int BL_ENA_ADELANTE = 31;
 const int BL_PWM_ATRAS = 8;        
 const int BL_PWM_ADELANTE = 7;
 
 
 
+// Es necesario poner los enables en cero cuando se frena?
 
-void initFR() {
+// LLANTA DELANTERA DERECHA.
+// Si velocidad cero, frena. 
+// Si velocidad negativa gira hacia atras.
+
+void frontRightWheel(int speed) {
+    if (speed >= 0) {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, 0);
+      analogWrite(FR_PWM_ADELANTE, speed);
+    }
+    else {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, speed);
+      analogWrite(FR_PWM_ADELANTE, 0);
+      }
+  }
+
+
+// LLANTA DELANTERA IZQUIERDA.
+// Si velocidad cero, frena. 
+// Si velocidad negativa gira hacia atras.
+
+void frontLeftWheel(int speed) {
+    if (speed >= 0) {
+      digitalWrite(FL_ENA_ATRAS, HIGH);
+      digitalWrite(FL_ENA_ADELANTE, HIGH);
+      analogWrite(FL_PWM_ATRAS, speed);
+      analogWrite(FL_PWM_ADELANTE, 0);
+    }
+    else {
+      digitalWrite(FL_ENA_ATRAS, HIGH);
+      digitalWrite(FL_ENA_ADELANTE, HIGH);
+      analogWrite(FL_PWM_ATRAS, 0);
+      analogWrite(FL_PWM_ADELANTE, speed);
+      }
+  }
+
+
+// FALTA EDITAR LAS DOS LLANTAS TRASERAS.
+
+// LLANTA DELANTERA DERECHA.
+// Si velocidad cero, frena. 
+// Si velocidad negativa gira hacia atras.
+
+void frontRightWheel(int speed) {
+    if (speed >= 0) {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, 0);
+      analogWrite(FR_PWM_ADELANTE, speed);
+    }
+    else {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, speed);
+      analogWrite(FR_PWM_ADELANTE, 0);
+      }
+  }
+
+// LLANTA DELANTERA DERECHA.
+// Si velocidad cero, frena. 
+// Si velocidad negativa gira hacia atras.
+
+void frontRightWheel(int speed) {
+    if (speed >= 0) {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, 0);
+      analogWrite(FR_PWM_ADELANTE, speed);
+    }
+    else {
+      digitalWrite(FR_ENA_ATRAS, HIGH);
+      digitalWrite(FR_ENA_ADELANTE, HIGH);
+      analogWrite(FR_PWM_ATRAS, speed);
+      analogWrite(FR_PWM_ADELANTE, 0);
+      }
+  }
+
+void goCar() {
+    frontRightWheel(80);
+    frontLeftWheel(80);
+  }
+
+void stopCar() {
+    frontRightWheel(0);
+    frontLeftWheel(0);
+  }
+
+void initWheels() {
   pinMode(FR_ENA_ATRAS, OUTPUT);
   pinMode(FR_ENA_ADELANTE, OUTPUT);
   pinMode(FR_PWM_ATRAS, OUTPUT);
   pinMode(FR_PWM_ADELANTE, OUTPUT);
-}
 
-void initFL() {
   pinMode(FL_ENA_ATRAS, OUTPUT);
   pinMode(FL_ENA_ADELANTE, OUTPUT);
   pinMode(FL_PWM_ATRAS, OUTPUT);
   pinMode(FL_PWM_ADELANTE, OUTPUT);
-}
 
-void avanzaFR() {
-  digitalWrite(FR_ENA_ATRAS, HIGH);
-  digitalWrite(FR_ENA_ADELANTE, HIGH);
-  analogWrite(FR_PWM_ATRAS, 0);
-  analogWrite(FR_PWM_ADELANTE, 100);
-}
+  pinMode(BR_ENA_ATRAS, OUTPUT);
+  pinMode(BR_ENA_ADELANTE, OUTPUT);
+  pinMode(BR_PWM_ATRAS, OUTPUT);
+  pinMode(BR_PWM_ADELANTE, OUTPUT);
 
-void avanzaFL() {
-  digitalWrite(FL_ENA_ATRAS, HIGH);
-  digitalWrite(FL_ENA_ADELANTE, HIGH);
-  analogWrite(FL_PWM_ATRAS, 100);
-  analogWrite(FL_PWM_ADELANTE, 0);
-}
-
-
-void frenaFR() {
-  analogWrite(FR_PWM_ATRAS, 0);
-  analogWrite(FR_PWM_ADELANTE, 0);
-  analogWrite(FR_PWM_ATRAS, 0);
-  analogWrite(FR_PWM_ADELANTE, 0);
-}
-
-void frenaFL() {
-  analogWrite(FL_PWM_ATRAS, 0);
-  analogWrite(FL_PWM_ADELANTE, 0);
-  analogWrite(FL_PWM_ATRAS, 0);
-  analogWrite(FL_PWM_ADELANTE, 0);
+  pinMode(BL_ENA_ATRAS, OUTPUT);
+  pinMode(BL_ENA_ADELANTE, OUTPUT);
+  pinMode(BL_PWM_ATRAS, OUTPUT);
+  pinMode(BL_PWM_ADELANTE, OUTPUT);
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  initFR();
-  initFL();
+  initWheels();
   delay(1000);
-  avanzaFR();
-  avanzaFL();
+  goCar();
   delay(1000);
-  frenaFR();
-  frenaFL();
+  stopCar();
 }
 
 void loop() {
